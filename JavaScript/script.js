c71dd10bbe7687ca8f912f9a9d3c698094a249c8
Fahrul187 <fahrulmuhammad764@gmail.com>
2025-07-16 22:31:03 +0700

    mengubah navbar ke versiku

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,11 +3,17 @@ const navbarNav = document.querySelector('.navbar-nav');
 const phHamburger = document.querySelector('.ph-hamburger');
 
 document.querySelector('.ph-hamburger').onclick = () => {
-    navbarNav.classList.toggle('active');
+    navbarNav.classList.toggle('hurung');
 }
 
 document.addEventListener('click', function (e) { 
     if (!phHamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
+        navbarNav.classList.remove('hurung');
     }
 });
+
+document.querySelectorAll('.navbar-nav a').forEach(link => {
+    navbarNav.addEventListener('click', () => {
+        navbarNav.classList.remove('hurung')
+    })
+})
